361e87a37afcd6922b3f74489c53a1357c9886de
atif0911 <sardaratifali2003@gmail.com>
2025-09-11 18:32:38 +0530

    starting a todo project

---
+++ Backend-PS/20-RestApi/server/index.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+const express = require("express");
+require("dotenv").config(); // load .env
+
+
+const rootDir = require("./utils/pathUtil");
+
+const MONGO_URL = process.env.MONGO_URL;
+
+const error = require("./controllers/error");
+const { default: mongoose } = require("mongoose");
+
+const app = express();
+
+app.use(express.urlencoded());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(error.notFoundController);
+
+const PORT = 3000;
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on address http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error while connecting to MongoDB ", err);
+  });
